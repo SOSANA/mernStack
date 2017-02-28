db97aa27d308ed1c70ef66ce5b3f34dce589744c
sosana <thesosana@gmail.com>
2017-02-28 13:38:30 -0800

    updating client folder

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,15 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const PATHS = {
+
+}
+
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'styled-components', 'react-redux', 'redux', 'reselect',
+  'axios', 'react-router'
+];
+
+export default {
+
+}
