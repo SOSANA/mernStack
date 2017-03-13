f4dd33d7f1b368c5b02a05e0a7d5337ddea56fa0
sosana <thesosana@gmail.com>
2017-03-13 15:58:54 -0700

    updating webpack dev

--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -1,9 +1,13 @@
-import webpack from 'webpack';
 import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const PATHS = {
-
-}
+  app: path.join(__dirname, 'client/index.js'),
+  build: path.join(__dirname, 'dist'),
+  style: path.join(__dirname, 'public', 'css'),
+};
 
 const VENDOR_LIBS = [
   'react', 'react-dom', 'styled-components', 'react-redux', 'redux', 'reselect',
@@ -11,5 +15,40 @@ const VENDOR_LIBS = [
 ];
 
 export default {
-
-}
+  devtool: 'eval',
+  entry: {
+    bundle: '.client/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    }),
+    new ExtractTextPlugin('style.css'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+};
