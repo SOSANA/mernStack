104279a882854daba3b2635ed3cc97f8a6370f37
sosana <thesosana@gmail.com>
2017-03-28 12:57:11 -0700

    updating server env console.logs

--- client/config/server/index.js
+++ client/config/server/index.js
@@ -3,8 +3,9 @@ import colors from 'colors';
 const isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
+  console.log('I am running in production'.green); // eslint-disable-line no-console
   require('./serverProd');
 }
 
-console.log('I am running in development'.blue); // eslint-disable-line no-console
+console.log('I am running in development'.orange); // eslint-disable-line no-console
 require('./serverDev');
